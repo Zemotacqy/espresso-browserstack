799a46f9fff0868c080be69bb23e2e483fce1c5b
Zemotacqy <mranjan1398@gmail.com>
2022-03-21 12:13:24 +0530

    chore: added testing snippets

--- app/src/androidTest/java/com/sample/browserstack/samplecalculator/EnsureOperationTests.java
+++ app/src/androidTest/java/com/sample/browserstack/samplecalculator/EnsureOperationTests.java
@@ -1,19 +1,30 @@
 package com.sample.browserstack.samplecalculator;
 
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.filters.MediumTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.squareup.spoon.Spoon;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.Assert.*;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.lang.reflect.Method;
 
 /**
  * Espresso tests to ensure that simple operations result in
@@ -44,6 +55,21 @@ public class EnsureOperationTests {
         childScreenshotMethod(screenshotName);
     }
 
+    private boolean checkScreenshotFile(String screenshotName, String testName) {
+        Log.d("Screenshot", "Entering");
+        File baseFile = new File(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)), "screenshots");
+//        File baseFile = new File(String.valueOf(ApplicationProvider.getApplicationContext().getExternalFilesDir(null)), "screenshots");
+        File classDir = new File(baseFile, "com.sample.browserstack.samplecalculator.EnsureOperationTests");
+        File testDir = new File(classDir, testName);
+        File[] fileList = testDir.listFiles();
+        if(fileList == null) return false;
+        for (int i = 0; i < fileList.length; i++) {
+            Log.d("Screenshot", "Filename: " + fileList[i].getAbsolutePath());
+            if(fileList[i].getName().endsWith(screenshotName + ".png")) return true;
+        }
+        return false;
+    }
+
     @Test
     public void ensureAdditionWorks() {
         onView(withId(R.id.buttonOne)).perform(click());
@@ -56,6 +82,10 @@ public class EnsureOperationTests {
 
         ScreenshotUtils screenshotUtils = new ScreenshotUtils();
         screenshotUtils.captureScreenshot("post_addition");
+
+        boolean result = checkScreenshotFile("post_addition", "ensureAdditionWorks");
+        Assert.assertEquals(true, result);
+
     }
 
     @Test
@@ -72,6 +102,9 @@ public class EnsureOperationTests {
         onView(withId(R.id.editText)).check(matches(withText("11")));
 
         screenshotUtils.captureScreenshot("post_subtraction");
+
+        boolean result = checkScreenshotFile("post_subtraction", "ensureSubtractionWorks");
+        Assert.assertEquals(true, result);
     }
 
     @Test
@@ -84,6 +117,9 @@ public class EnsureOperationTests {
         onView(withId(R.id.editText)).check(matches(withText("60")));
 
         childScreenshotMethod("post_multiplication");
+
+        boolean result = checkScreenshotFile("post_multiplication", "ensureMultiplicationWorks");
+        Assert.assertEquals(true, result);
     }
 
     @Test
@@ -96,5 +132,8 @@ public class EnsureOperationTests {
         onView(withId(R.id.editText)).check(matches(withText("4")));
 
         parentScreenshotMethod("post_division");
+
+        boolean result = checkScreenshotFile("post_division", "ensureDivisionWorks");
+        Assert.assertEquals(true, result);
     }
 }
